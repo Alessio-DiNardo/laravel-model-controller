dd939911e3bb953a164aff6baf5f2a5e626c35e3
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-07-23 15:42:00 +0200

    fix es

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield ('title', 'Laravel App')</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title','Hello World')</title>
+
+    @vite('resources/js/app.js')
+    @yield('custom-stylesheets')
+    @yield('custom-script-head')
 </head>
 <body>
-    
+    @include('partials.header')
     <main>
-        @yield('main-content')
+        @yield('main-section')
     </main>
+    @include('partials.footer')
+    {{-- @include('main-content') --}}
+    @yield('custom-scripts-tail')
 
     @vite('resources/js/app.js')
 </body>
-</html>
+</html> 
